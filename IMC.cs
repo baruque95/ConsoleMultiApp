c02b03764014676044aefb63b0ecc5feee263e55
Gabriel Baruque <baruque@edu.unirio.br>
2021-10-16 20:33:25 -0300

    Adicionar arquivos de projeto.

---
+++ IMC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMultiApp
+{
+    class IMC
+    {
+        int getPeso()
+        {
+            int peso;
+            Console.WriteLine("Digite o seu peso: ");
+            peso = Convert.ToInt32(Console.ReadLine());
+
+            return peso;
+        }
+
+        int calculaIMC(int peso, int altura)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleMultiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao;
+
+            Console.WriteLine("##########################");
+            Console.WriteLine("# BEM-VINDX AO MULTI APP #");
+            Console.WriteLine("# Escolha uma função:    #\n");
+            Console.WriteLine("# (1) Cálculo do IMC;    #\n");
+            Console.WriteLine("# (2) Salários mínimos;  #\n");
+            Console.WriteLine("# (3) Volume da esfera;  #\n");
+            Console.WriteLine("# (4) Média dos alunos;  #\n");
+            Console.WriteLine("# (5) Pets;              #\n");
+            Console.WriteLine("# (6) Encerrar programa; #\n");
+
+            opcao = Convert.ToInt32(Console.ReadLine());
+
+            // Programa quebra feio se coloca letra no input de opção!!
+
+            if (opcao > 0 && opcao < 7)
+            {
+                switch (opcao)
+                {
+                    case 1:
+                        // IMC
+                        
+                        break;
+                    case 2:
+                        // SALARIO
+                        Console.WriteLine("Salários mínimos");
+                        break;
+                    case 3:
+                        // VOLUME ESFERA
+                        Console.WriteLine("Volume da esfera");
+                        break;
+                    case 4:
+                        // MEDIA ALUNOS
+                        Console.WriteLine("Calculo da média");
+                        break;
+                    case 5:
+                        // PETS
+                        Console.WriteLine("Cadastro de Pets");
+                        break;
+                    case 6:
+                        // ENCERRAR
+                        Console.WriteLine("Programa encerrado");
+                        break;
+                }
+            } else
+            {
+                Console.WriteLine("Comando inválido.");
+            }
+        }
+    }
+}
